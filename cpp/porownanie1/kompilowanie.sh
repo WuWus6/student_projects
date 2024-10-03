873f2e221d6dad7b02491b52ab66bc466f670fe5
WuWus6 <150009785+WuWus6@users.noreply.github.com>
2024-10-03 08:12:12 +0200

    Update kompilowanie.sh

--- cpp/porownanie1/kompilowanie.sh
+++ cpp/porownanie1/kompilowanie.sh
@@ -1,32 +1,12 @@
 #!/bin/sh
+rm main_test
+clear
+echo "----Kompilacja benchmarka testujacego----"
 
-# Usuwanie istniejącego pliku main, jeśli istnieje
-if [ -f main ]; then
-  rm main
-fi
+g++ -std=c++11 tester.cpp -o main_test -lgtest_main -lgtest -lbenchmark -lpthread
 
-# Usuwanie pliku error_log.txt, jeśli istnieje
-if [ -f error_log.txt ]; then
-  rm error_log.txt
-fi
-
-echo "----Kompilacja programu----"
-
-# Kompilacja z przekierowaniem błędów do pliku error_log.txt
-g++ -o main tester.cpp 2> error_log.txt
-
-# Sprawdzanie, czy plik error_log.txt jest pusty
-if [ -s error_log.txt ]; then
-  echo "---Błędy podczas kompilacji---"
-  # Wyświetlanie błędów na ekranie z paginacją
-  more error_log.txt
-else
-  echo "---Kompilacja ukończona pomyślnie---"
-  echo "---Uruchamianie programu---"
-  chmod +x main
-  ./main
-  echo "---Zakończono program---"
-  
-  # Usunięcie pliku error_log.txt, ponieważ nie było błędów
-  rm error_log.txt
-fi
+echo "---Kompilacja benchmarka ukonczona----"
+echo "---Uruchamianie beanchmarka---"
+chmod +x main_test
+./main_test
+echo "---Zakonczono benchmark---"
