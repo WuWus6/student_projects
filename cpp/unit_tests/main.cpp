32c5a79cf59f415d33047ce135174fb1d787eb9a
WuWus6 <150009785+WuWus6@users.noreply.github.com>
2024-11-14 02:38:47 +0100

    Update main.cpp

--- cpp/unit_tests/main.cpp
+++ cpp/unit_tests/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <string>
+#include "functions.cpp"
 
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Podaj liczbę: ";
+    cin >> n;
+
+    string result = fizzBuzz(n);
+    cout << result << endl;
+
+    return 0;
+}
