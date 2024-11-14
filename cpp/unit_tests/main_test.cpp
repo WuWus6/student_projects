3ae3b7f7281213d9e6cb59237b051ac3d7922c87
WuWus6 <150009785+WuWus6@users.noreply.github.com>
2024-11-14 02:39:13 +0100

    Update main_test.cpp

--- cpp/unit_tests/main_test.cpp
+++ cpp/unit_tests/main_test.cpp
@@ -1,1 +1,32 @@
+#include <gtest/gtest.h>
+#include "functions.cpp"
 
+// Test dla liczby podzielnej przez 3
+TEST(FizzBuzzTest, MultiplesOf3) {
+    ASSERT_EQ(fizzBuzz(3), "Fizz");
+    ASSERT_EQ(fizzBuzz(9), "Fizz");
+}
+
+// Test dla liczby podzielnej przez 5
+TEST(FizzBuzzTest, MultiplesOf5) {
+    ASSERT_EQ(fizzBuzz(5), "Buzz");
+    ASSERT_EQ(fizzBuzz(10), "Buzz");
+}
+
+// Test dla liczby podzielnej przez 3 i 5
+TEST(FizzBuzzTest, MultiplesOf3And5) {
+    ASSERT_EQ(fizzBuzz(15), "FizzBuzz");
+    ASSERT_EQ(fizzBuzz(30), "FizzBuzz");
+}
+
+// Test dla liczby, która nie jest podzielna przez 3 ani 5
+TEST(FizzBuzzTest, NeitherMultipleOf3Nor5) {
+    ASSERT_EQ(fizzBuzz(1), "1");
+    ASSERT_EQ(fizzBuzz(7), "7");
+}
+
+// Inicjalizacja testów
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
