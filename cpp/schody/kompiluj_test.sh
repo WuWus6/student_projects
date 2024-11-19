c21ce9c2acadfceaa7094b19ccd62623845e7a57
WuWus6 <150009785+WuWus6@users.noreply.github.com>
2024-11-19 22:03:42 +0100

    Update kompiluj_test.sh

--- cpp/schody/kompiluj_test.sh
+++ cpp/schody/kompiluj_test.sh
@@ -1,1 +1,16 @@
+#!/bin/sh
+rm tester
+clear
+echo "----Kompilacja programu testujacego----"
 
+# Kompilacja kodu źródłowego dla programu głównego
+g++ main.cpp -o main
+
+# Kompilacja kodu testów
+g++ tester.cpp -o tester -lgtest -lgtest_main -pthread
+
+echo "---Kompilacja testow ukonczona----"
+echo "---Uruchamianie programu testujacego---"
+chmod +x tester
+./tester
+echo "---Zakonczono program testujacy---"
