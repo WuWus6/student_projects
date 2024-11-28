7e2233add8e97fdd11c300690987ef68fec77d4b
WuWus6 <150009785+WuWus6@users.noreply.github.com>
2024-11-28 08:08:16 +0100

    Update kompiluj_test.sh

--- cpp/schody/kompiluj_test.sh
+++ cpp/schody/kompiluj_test.sh
@@ -4,7 +4,7 @@ clear
 echo "----Kompilacja programu testujacego----"
 
 # Kompilacja kodu źródłowego dla programu głównego
-g++ main.cpp -o main
+g++ schody.cpp -o main
 
 # Kompilacja kodu testów
 g++ tester.cpp -o tester -lgtest -lgtest_main -pthread
